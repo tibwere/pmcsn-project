1f9690fff2e61b07828ca7c437457bb0e612538b
alessandrochillotti <alessandro.chillotti@outlook.it>
2021-07-16 13:01:12 +0200

    Separata gestione dei tempi di sistema dalla lista degli eventi

--- src/simul.c
+++ src/simul.c
@@ -54,17 +54,20 @@
 typedef struct event_list {
     double arrivals[NUMBER_OF_QUEUES]; 
     double gen_completions[M-1]; 
-    double ded_completion;
-    double next;                      
-    double current;
-    double last;                    
+    double ded_completion;                  
 } event_list_t; 
 
-struct time_integrated_populations {
+typedef struct times {
+    double next;                      
+    double current;
+    double last;  
+} times_t;
+
+typedef struct time_integrated_populations {
     double customers[NUMBER_OF_QUEUES];  
     double queue[NUMBER_OF_QUEUES];      
     double service[NUMBER_OF_QUEUES];
-};
+} time_integrated_populations_t;
 
 /* System Status */
 int customers[NUMBER_OF_QUEUES] = {[0 ... NUMBER_OF_QUEUES - 1] = 0};
@@ -72,8 +75,10 @@ int gen_status[M-1] = {[0 ... M - 2] = IDLE};
 int ded_status = IDLE;
 
 /* Event List */
-event_list_t *t;
+event_list_t *events;
 
+/* Simulation Clock */
+times_t *t;
 
 int integers_sum(int *items, int len) {
     int sum = 0;
@@ -114,37 +119,37 @@ double next_event(int *min_event_type, int *min_index)
     int min_arrival_index, min_gen_compl_index;
     double min_arrival, min_gen_compl;
 
-    min_arrival = min_from_array(t->arrivals, NUMBER_OF_QUEUES, &min_arrival_index);
-    min_gen_compl = min_from_array(t->gen_completions, M-1, &min_gen_compl_index);
+    min_arrival = min_from_array(events->arrivals, NUMBER_OF_QUEUES, &min_arrival_index);
+    min_gen_compl = min_from_array(events->gen_completions, M-1, &min_gen_compl_index);
 
     //printf("min_arr = %lf, min_compl = %lf")
 
     if (min_arrival < min_gen_compl) {
-        if (min_arrival < t->ded_completion) {
+        if (min_arrival < events->ded_completion) {
             *min_event_type = ARR_EVENT_TYPE;
             *min_index = min_arrival_index;
             return min_arrival;
         } else {
             *min_event_type = DED_COMPL_EVENT_TYPE;
             *min_index = 0;
-            return t->ded_completion;
+            return events->ded_completion;
         }
     } else {
-        if (min_gen_compl < t->ded_completion) {
+        if (min_gen_compl < events->ded_completion) {
             *min_event_type = GEN_COMPL_EVENT_TYPE;
             *min_index = min_gen_compl_index;
             return min_gen_compl;
         } else {
             *min_event_type = DED_COMPL_EVENT_TYPE;
             *min_index = 0;
-            return t->ded_completion;
+            return events->ded_completion;
         }        
     }
 }
 
 double GetArrival(int type)
 {
-    static double arrivals[NUMBER_OF_QUEUES] = {START};
+    static double arrivals[NUMBER_OF_QUEUES] = {[0 ... NUMBER_OF_QUEUES - 1] = START};
 
     switch (type)
     {
@@ -194,17 +199,17 @@ double GetService(int type)
     case SR_STD:
         SelectStream(SPED_RIT_SERV_STREAM);
         return (Exponential(1 / MU_SR));
-    default:
+    default: 
         abort();
     }
-}  
+}
 
-struct time_integrated_populations *init_tip(void)
+time_integrated_populations_t *init_tip(void)
 {
-    struct time_integrated_populations *p = malloc(sizeof(struct time_integrated_populations));
+    time_integrated_populations_t *p = malloc(sizeof(time_integrated_populations_t));
     if (p == NULL)
         return NULL;
-    memset(p, 0x0, sizeof(struct time_integrated_populations));
+    memset(p, 0x0, sizeof(time_integrated_populations_t));
 
     return (p);
 }
@@ -213,20 +218,28 @@ void init_event_list(void)
 {
 
     int i;
-    t = malloc(sizeof(event_list_t));
-    if (t == NULL)
+    events = malloc(sizeof(event_list_t));
+    if (events == NULL)
         abort();
-    memset(t, 0x0, sizeof(event_list_t));
-
-    t->current = START;
+    memset(events, 0x0, sizeof(event_list_t));
 
     for (i = 0; i < NUMBER_OF_QUEUES; ++i) 
-        t->arrivals[i] = GetArrival(i);
+        events->arrivals[i] = GetArrival(i);
 
     for (i = 0; i < M - 1; ++i) 
-        t->gen_completions[i] = INFTY;
+        events->gen_completions[i] = INFTY;
     
-    t->ded_completion = INFTY;
+    events->ded_completion = INFTY;
+}
+
+void init_times(void) 
+{
+    t = malloc(sizeof(times_t));
+    if (t == NULL)
+        abort();
+    memset(t, 0x0, sizeof(times_t));
+
+    t->current = START;
 }
 
 int *get_in_service_per_type(void)
@@ -244,7 +257,7 @@ int *get_in_service_per_type(void)
     return (in_service_per_type);
 }
 
-void update_tip(struct time_integrated_populations *area)
+void update_tip(time_integrated_populations_t *area)
 {
     int *in_service_per_type = get_in_service_per_type();
 
@@ -286,18 +299,18 @@ void toggle_server_status(int index)
     if (max_prio_queue_not_empty != -1) {
         if (index != -1) {
             gen_status[index] = max_prio_queue_not_empty;
-            t->gen_completions[index] = t->current + GetService(max_prio_queue_not_empty);
+            events->gen_completions[index] = t->current + GetService(max_prio_queue_not_empty);
         } else {
             ded_status = max_prio_queue_not_empty;
-            t->ded_completion = t->current + GetService(max_prio_queue_not_empty);
+            events->ded_completion = t->current + GetService(max_prio_queue_not_empty);
         }
     } else {
         if (index != -1) {
             gen_status[index] = IDLE;
-            t->gen_completions[index] = INFTY;
+            events->gen_completions[index] = INFTY;
         } else {
             ded_status = IDLE;
-            t->ded_completion = INFTY;
+            events->ded_completion = INFTY;
         }
     }
 }
@@ -307,10 +320,10 @@ void next_assignment_ded_server(void)
     int *in_service = get_in_service_per_type();
     if (customers[SR_BP] > in_service[SR_BP]) {
         ded_status = SR_BP;
-        t->ded_completion = t->current + GetService(SR_BP);
+        events->ded_completion = t->current + GetService(SR_BP);
     } else if (customers[SR_STD] > in_service[SR_STD]) {
         ded_status = SR_STD;
-        t->ded_completion = t->current + GetService(SR_STD);
+        events->ded_completion = t->current + GetService(SR_STD);
     } else {
         toggle_server_status(-1);
     }
@@ -327,9 +340,9 @@ void print_update(void)
     };
 
     printf("Current time                 = %12.6lf\n", t->current);
-    printf("Next arrival time            = %12.6lf\n", min_from_array(t->arrivals, NUMBER_OF_QUEUES, &dummy));
-    printf("Next gp_completion           = %12.6lf\n", min_from_array(t->gen_completions, M-1, &dummy));
-    printf("Next ded_completion          = %12.6lf\n\n", t->ded_completion);
+    printf("Next arrival time            = %12.6lf\n", min_from_array(events->arrivals, NUMBER_OF_QUEUES, &dummy));
+    printf("Next gp_completion           = %12.6lf\n", min_from_array(events->gen_completions, M-1, &dummy));
+    printf("Next ded_completion          = %12.6lf\n\n", events->ded_completion);
     
     for (j = 0; j < NUMBER_OF_QUEUES; j++)
         printf("Customers of %-15s = %5d\n", types_of_service[j], customers[j]);
@@ -339,18 +352,18 @@ void print_update(void)
     for (j = 0; j < M-1; j++) {
         tmp = (gen_status[j] == -1) ? "IDLE" : types_of_service[gen_status[j]];
         printf("Gp_Server[%d] Status = %s \n", j + 1, tmp);
-        printf("             Next   = %lf\n", t->gen_completions[j]);
+        printf("             Next   = %lf\n", events->gen_completions[j]);
     }
 
     tmp = (ded_status == -1) ? "IDLE" : types_of_service[ded_status];
     printf("Ded_Server   Status = %s \n", tmp);
-    printf("             Next   = %lf\n", t->ded_completion);
+    printf("             Next   = %lf\n", events->ded_completion);
 
     printf("===========================================\n");
     //getchar();
 }
 
-void print_report(int *number_of_completions, struct time_integrated_populations *area) 
+void print_report(int *number_of_completions, time_integrated_populations_t *area) 
 {
     int tot_completions = integers_sum(number_of_completions, NUMBER_OF_QUEUES);
     double area_customers = doubles_sum(area->customers, NUMBER_OF_QUEUES);
@@ -396,9 +409,9 @@ int has_to_continue(int *flags)
 int main(void) 
 {
     int number_of_completions[NUMBER_OF_QUEUES];
-    struct time_integrated_populations *area;
+    time_integrated_populations_t *area;
     int idle_server_index;
-    int continue_simul[NUMBER_OF_QUEUES] = {1};
+    int continue_simul[NUMBER_OF_QUEUES] = {[0 ... NUMBER_OF_QUEUES - 1] = 1};
     int next_event_type;
     int next_event_index;
     int current_state;
@@ -407,6 +420,7 @@ int main(void)
 
     memset(number_of_completions, 0x0, NUMBER_OF_QUEUES * sizeof(int));
     init_event_list();
+    init_times();
     area = init_tip();
 
     for (int i = 0; i < M-1; ++i) {
@@ -425,11 +439,11 @@ int main(void)
         if (next_event_type == ARR_EVENT_TYPE) {                       /* ARRIVO */
             customers[next_event_index]++;
 
-            t->arrivals[next_event_index] = GetArrival(next_event_index);
+            events->arrivals[next_event_index] = GetArrival(next_event_index);
             
-            if (t->arrivals[next_event_index] > STOP) {
+            if (events->arrivals[next_event_index] > STOP) {
                 continue_simul[next_event_index] = 0;
-                t->arrivals[next_event_index] = INFTY;
+                events->arrivals[next_event_index] = INFTY;
 
                 if (!has_to_continue(continue_simul))
                     t->last = t->current;
@@ -441,7 +455,7 @@ int main(void)
             } else {
                 if (ded_status == IDLE) {
                     ded_status = next_event_index;
-                    t->ded_completion = t->current + GetService(next_event_index);
+                    events->ded_completion = t->current + GetService(next_event_index);
                 }
             }            
 
